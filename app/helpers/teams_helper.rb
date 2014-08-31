9762a23f14c94fa17b036bd54e7d0a3e364b9ec7
Yours <User@users-mbp.gateway.2wire.net>
2014-08-30 20:13:03 -0400

    Initial commit

---
+++ app/helpers/teams_helper.rb
@@ -0,0 +1,59 @@
+module TeamsHelper
+
+	def generate_teams
+
+		unassigned_students = []
+
+		Student.all.each do |student|
+
+			unassigned_students.push(student.name)
+
+		end
+
+		num_teams = params[:num].to_i
+
+		team_size = unassigned_students.count / num_teams
+
+		teams = []
+
+		until teams.count == num_teams
+
+			teammates = unassigned_students.sample(team_size)
+
+			teams.push(teammates)
+
+			teammates.each do |student|
+
+				unassigned_students.delete(student)
+
+			end
+
+		end
+
+		unassigned_students.each_with_index do |student, index|
+
+			teams[index].push(student)
+
+		end
+
+		return teams
+	end
+
+	def save_teams
+
+    Team.destroy_all # discard any earlier teams
+
+    if @teams != nil
+
+      @teams.each do |team|
+
+      @leader = rand(0...team.count)
+
+      team[@leader] += '*'
+
+      Team.create(members: team.join(', '))
+
+      end
+    end
+  end
+end
