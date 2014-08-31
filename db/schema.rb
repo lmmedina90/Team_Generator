595dcf52f9a17a05df89b5962b168036504212ed
Yours <User@users-mbp.gateway.2wire.net>
2014-08-30 20:32:37 -0400

    Update migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140820131356) do
+ActiveRecord::Schema.define(version: 20140820011347) do
 
   create_table "students", force: true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "teams", force: true do |t|
-    t.string   "members"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
